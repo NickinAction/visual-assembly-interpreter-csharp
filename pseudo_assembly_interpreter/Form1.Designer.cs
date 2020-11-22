e90aac5c763198e7c6c24f310940d2f6d7c4673e
Nikita <nikitagon5@gmail.com>
2020-11-22 14:02:07 -0500

    Initial commit + add register labels

---
+++ pseudo_assembly_interpreter/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace pseudo_assembly_interpreter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.execute_button = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // execute_button
+            // 
+            this.execute_button.Location = new System.Drawing.Point(12, 12);
+            this.execute_button.Name = "execute_button";
+            this.execute_button.Size = new System.Drawing.Size(96, 53);
+            this.execute_button.TabIndex = 0;
+            this.execute_button.Text = "Execute";
+            this.execute_button.UseVisualStyleBackColor = true;
+            this.execute_button.Click += new System.EventHandler(this.execute_button_clicked);
+            // 
+            // flowLayoutPanel1
+            // 
+            
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(553, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(235, 417);
+            this.flowLayoutPanel1.TabIndex = 1;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // register_label1
+            // 
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.execute_button);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button execute_button;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label[] register_labels;
+    }
+}
+
---
+++ pseudo_assembly_interpreter/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pseudo_assembly_interpreter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            register_labels = new System.Windows.Forms.Label[20];
+            for (int i = 0; i < 16; i++)
+            {
+                this.register_labels[i] = new System.Windows.Forms.Label();
+                this.flowLayoutPanel1.Controls.Add(this.register_labels[i]);
+
+                this.register_labels[i].AutoSize = true;
+                this.register_labels[i].Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                this.register_labels[i].Location = new System.Drawing.Point(3, 0);
+                this.register_labels[i].MaximumSize = new System.Drawing.Size(100, 100);
+                this.register_labels[i].MinimumSize = new System.Drawing.Size(200, 0);
+                this.register_labels[i].Name = "register_label" + i;
+                this.register_labels[i].Size = new System.Drawing.Size(200, 26);
+                this.register_labels[i].TabIndex = 2;
+                this.register_labels[i].Text = "R" + i + ": ";
+            }
+        }
+
+        private void execute_button_clicked(object sender, EventArgs e)
+        {
+            Console.Out.WriteLine("Execute Button was Clicked");
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
